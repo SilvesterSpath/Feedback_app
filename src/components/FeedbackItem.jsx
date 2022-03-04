2e6c135883605c3f3b36e6a844d78ba6a876af84
SilvesterSpath <spath.sz@gmail.com>
2022-03-04 07:35:06 +0100

    Card component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,18 +1,21 @@
-import {useState} from 'react'
+import Card from "../shared/Card"
+import PropTypes from 'prop-types'
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('This is an example of a feedback item')
-
-
-
-
-  return (
-    <div className="card">
-      <div className="num-display">{rating}</div>  
-      <div className="text-display">{text}</div>      
-    </div>
+function FeedbackItem({item}) {
+  return (    
+     <Card >
+      <div className="num-display">{item.rating}</div>  
+      <div className="text-display">{item.text}</div>      
+    </Card>
   )
+}
+
+Card.defaultProps = {
+  reverse: false
+}
+
+Card.propTypes = {
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,29 @@
+import FeedbackItem from "./FeedbackItem"
+import PropTypes from 'prop-types'
+
+function FeedbackList({feedback}) {
+  if(!feedback || feedback.length === 0){
+    return <p>No Feedback Yet</p>
+  }
+
+  return (
+    <div className="feedback-list">   
+     {feedback.map((item, idx)=>(
+      <FeedbackItem key={idx} item={item}/>
+    ))}
+    </div>
+
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
+  )
+}
+
+export default FeedbackList
