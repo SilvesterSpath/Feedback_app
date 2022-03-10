7faa2fbf9a95fc55aca02927e9d8b09fad2df6e3
SilvesterSpath <spath.sz@gmail.com>
2022-03-10 07:08:58 +0100

    disable button

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,10 +5,19 @@ import Button from "../shared/Button"
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisables] = useState(true)
+  const [message, setMessage] = useState('')
 
-  const handleTextChange = (e)=>{    
+  const handleTextChange = (e)=>{        
+    if (text.length >= 10 || text.length <= 1){
+      setBtnDisables(false)
+      setMessage(null)
+    } else {
+      setBtnDisables(true)
+      setMessage('Please write at least 10 chars')
+    }
+
     setText(e.target.value)
-    console.log(text);
   }
 
   return (
@@ -18,8 +27,9 @@ function FeedbackForm() {
         {/* @TODO - rating select component */}
         <div className="input-group">
           <input type="text" placeholder="Write a review" value={text} onChange={(e)=> handleTextChange(e)}/>
-          <Button type="submit" >Send</Button>
+          <Button type="submit" isDisabled={btnDisabled} >Send</Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>    
   )
