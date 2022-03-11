fad2e2d85cef0a5eb993a37b4e7c74f94a85e10a
SilvesterSpath <spath.sz@gmail.com>
2022-03-11 07:55:19 +0100

    clear up add.js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react';
-
-import FeedbackData from '../data/FeedbackData';
 import FeedbackList from '../components/FeedbackList';
 import FeedbackStats from '../components/FeedbackStats';
 import FeedbackForm from '../components/FeedbackForm';
 
 function Home() {
-  const [feedbacks, setFeedbacks] = useState(FeedbackData);
-
-
-  return (
+    return (
     <>
       <FeedbackForm />
       <FeedbackStats />
